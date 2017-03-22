de03dc189dd25f4e555337429ad0c11a2facfc44
zzy131250 <zzy131250@outlook.com>
2017-03-22 11:57:12 +0000

    modify find_word.py

--- preprocess/find_word.py
+++ preprocess/find_word.py
@@ -5,14 +5,18 @@ try:
 except ImportError:
     import xml.etree.ElementTree as ET
 
-root_dir = u'C:\\Users\\J\\Desktop\\共享文书\\民事一审'
+root_dir = '/home/zzy131250/civil/'
+dest_dir = '/home/zzy131250/referee_system/preprocess/case_list/'
 files = listdir(root_dir)
+case_txt = open(dest_dir + 'marriage_law_case.txt', 'wb')
 
 for file in files:
-    tree = ET.ElementTree(file=root_dir + '\\' + file)
+    tree = ET.ElementTree(file=root_dir + file)
     for elem in tree.iterfind('.//QW'):
         for attr in elem.attrib:
             if attr == 'value':
                 content = elem.attrib[attr]
-                if content.find(u'民法通则') != -1:
-                    print file
+                if content.find(u'') != -1:
+                    case_txt.write(file)
+
+case_txt.close()
