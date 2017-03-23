9b59c06923b5583427364a09b781898c4fbd8154
Justin <zzy131250@outlook.com>
2017-03-23 16:57:29 +0800

    add tool files

--- preprocess/find_word.py
+++ preprocess/find_word.py
@@ -5,19 +5,20 @@ try:
 except ImportError:
     import xml.etree.ElementTree as ET
 
-root_dir = '/home/zzy131250/civil/'
-dest_dir = '/home/zzy131250/referee_system/preprocess/case_list/'
+# 根据文书涉及的法律对文书进行分类
+root_dir = u'C:\\Users\\J\\Desktop\\共享文书\\民事一审\\'
+dest_dir = 'C:\\Users\\J\\OneDrive\\Workspace\\PythonWorkspace\\referee_system\\preprocess\\case_list\\'
 files = listdir(root_dir)
-case_txt = open(dest_dir + 'road_traffic_case.txt', 'wb')
+case_txt = open(dest_dir + 'civil_law_case.txt', 'wb')
 
-# 婚姻法、合同法、民法通则、道理交通安全法
+# 婚姻法、合同法、民法通则
 for file in files:
     tree = ET.ElementTree(file=root_dir + file)
     for elem in tree.iterfind('.//QW'):
         for attr in elem.attrib:
             if attr == 'value':
                 content = elem.attrib[attr]
-                if content.find(u'道理交通安全法') != -1:
+                if content.find(u'民法通则') != -1:
                     case_txt.write(file + '\n')
 
 case_txt.close()
