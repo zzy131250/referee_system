4b3e4c6b8afebef3a9fdb33addcf59f1ecf4a62c
Justin <zzy131250@outlook.com>
2017-03-29 17:28:24 +0800

    data prepare done

--- preprocess/generate_not_applicable_instrument.py
+++ preprocess/generate_not_applicable_instrument.py
@@ -2,14 +2,29 @@
 from db_operation import Mongo
 from string_process import find_similar_law
 
-mongo = Mongo()
-mongo.set_collection('instrument')
+mongo_instrument = Mongo()
+mongo_instrument.set_collection('instrument')
+mongo_law = Mongo()
+mongo_law.set_collection('law')
 
 not_applicable_instrument_array = []
-for instrument in mongo.find_data({'applicable': 'yes'}):
+for instrument in mongo_instrument.find_data({'applicable': 'yes'}):
     not_applicable_instrument = {}
     not_applicable_instrument['case'] = instrument['case']
     not_applicable_instrument['applicable'] = 'no'
-    similar_law = find_similar_law(instrument['law_id'])
-    print similar_law['law_id_previous']
-    # print instrument['law_id'] + ', ' + law_id
+    result = mongo_law.find_data({'law_id': instrument['law_id']})
+    if result.count() == 1:
+        similar_law = find_similar_law(result[0]['law_id'])
+        law_previous = mongo_law.find_data({'law_id': similar_law['law_id_previous']})
+        law_next = mongo_law.find_data({'law_id': similar_law['law_id_next']})
+        if law_previous.count() == 0:
+            not_applicable_instrument['law_id'] = law_next[0]['law_id']
+        elif law_next.count() == 0:
+            not_applicable_instrument['law_id'] = law_previous[0]['law_id']
+        else:
+            if len(law_previous[0]['law_content']) < len(law_next[0]['law_content']):
+                not_applicable_instrument['law_id'] = law_next[0]['law_id']
+            else:
+                not_applicable_instrument['law_id'] = law_previous[0]['law_id']
+        not_applicable_instrument_array.append(not_applicable_instrument)
+mongo_instrument.insert_many(not_applicable_instrument_array)
